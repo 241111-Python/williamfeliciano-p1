59f652a15f69108196a7184d03b370bfcd0ef698
wfeliciano20 <william.feliciano2@gmail.com>
2024-11-15 12:53:13 -0500

    remove extra function

--- functions-library.sh
+++ functions-library.sh
@@ -6,19 +6,5 @@ function get_random_choice(){
     echo $(( RANDOM % $one_above_the_number_of_choices ))
 }
 
-function detect_OS_Environment(){
-    PATH_OS_LINUX=$1
-    PATH_OS_WINDOWS=$2
-    # Check if running on Ubuntu or GitBash on Windows
-if [ -d "$PATH_OS_LINUX" ];
-then
-    # I am on Linux
-    echo "$PATH_OS_LINUX"
-    
-else
-    # I am using Windows
-   echo "$PATH_OS_WINDOWS"
-fi
-}
 
 "$@"
